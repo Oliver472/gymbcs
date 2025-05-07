9b2e972a787a4f1cb50a2c0cf1518be4f1586cc8
Oliver Mrovčák <oliver.mrovcak@prosoft.sk>
2025-05-07 14:19:38 +0200

    refactored colors

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,18 +1,5 @@
 const config = {
     plugins: ["@tailwindcss/postcss"],
-    extend: {
-        colors: {
-            primary: "#FFFFFF",
-            secondary: "#E9E9E9",
-            accent: "#FF4410",
-            accent2: "#000000",
-            success: "#3C9E00",
-            danger: "#FF0000",
-        },
-        fontFamily: {
-            openSans: "Open Sans",
-        }
-    }
 };
 
 export default config;
